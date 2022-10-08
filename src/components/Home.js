e15f36b572c2483a0dda079daf9cfc11d3e2f46d
DevZsaosao <huyvu10012003@gmail.com>
2022-10-09 02:52:17 +0700

    up date redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,37 @@
-function Home() {
+import { connect } from 'react-redux';
+function Home(props) {
+    const handleDelete = (user) => {
+        props.deleteUserRedux(user);
+    };
+
     return (
         <div>
             <div>this is the page Home</div>
+            {props.dataRedux.map((user) => (
+                <div key={user.id}>
+                    {user.id} {user.name}
+                    <button onClick={() => handleDelete(user)}>Delete</button>
+                </div>
+            ))}
         </div>
     );
 }
+// get data
+const mapStateToProps = (state) => {
+    return {
+        dataRedux: state.users,
+    };
+};
+// create a function that allows firing action
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (user) =>
+            //firing action
+            dispatch({
+                type: 'DELETE_USER_REDUX',
+                payload: user,
+            }),
+    };
+};
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store/reducer/rootReducer.js
+++ src/store/reducer/rootReducer.js
@@ -5,8 +5,15 @@ const init = {
     ],
 };
 
-function rootReducer(state = init, dispatch) {
-    return state;
+function rootReducer(state = init, action) {
+    switch (action.type) {
+        case `DELETE_USER_REDUX`:
+            const newUser = state.users;
+            const users = newUser.filter((user) => user.id != action.payload.id);
+            return { ...state, users };
+        default:
+            return state;
+    }
 }
 
 export default rootReducer;
